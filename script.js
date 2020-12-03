c77ad55126c143c20d5b9eed1559906a0e2c593a
Richa Purba <richathorne@gmail.com>
2020-12-04 04:03:44 +0700

    Add navbar

--- script.js
+++
@@ -1,7 +0,0 @@
-// Everything is loaded including images.
-$(window).on("load", function(){
-
-    // Render the page on modern browser only.
-    if(renderPage) {
-      // Remove loader
-        $('body').addClass('loaded');}})
